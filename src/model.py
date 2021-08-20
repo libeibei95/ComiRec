fix bug: add summary of gradients

src/model.py
@@ -254,9 +254,6 @@ class Model_ComiRec_SA(Model):
             item_att_w = tf.layers.dense(item_hidden, num_heads, activation=None)
             item_att_w = tf.transpose(item_att_w, [0, 2, 1])
 
-            tf.summary.histogram('item_hidden', item_hidden)
-            tf.summary.histogram('item_att_w', item_att_w)
-
             atten_mask = tf.tile(tf.expand_dims(self.mask, axis=1), [1, num_heads, 1])
             paddings = tf.ones_like(atten_mask) * (-2 ** 32 + 1)
 
@@ -264,6 +261,9 @@ class Model_ComiRec_SA(Model):
             item_att_w = tf.nn.softmax(item_att_w)
 
             interest_emb = tf.matmul(item_att_w, item_list_emb)
+
+            tf.summary.histogram('item_att_w', item_att_w)
+            tf.summary.histogram('interest_emb', interest_emb)
 
         self.user_eb = interest_emb
         self.batch_size = batch_size
@@ -314,8 +314,10 @@ class Model_ComiRec_SA(Model):
         self.loss = tf.reduce_mean(self.loss_raw)
         #self.loss = self.loss + self.get_cl_loss()
         self.opt = tf.train.AdamOptimizer(learning_rate=self.lr)
-        grads = tf.gradients(self.loss, tf.trainable_variables())
-        for grad, var in zip(grads, tf.trainable_variables()):
+
+        tf.summary.histogram('loss_raw', self.loss_raw)
+        grads = self.opt.compute_gradients(self.loss, tf.trainable_variables())
+        for grad, var in grads:
             tf.summary.histogram(var.name+'_grad', grad)
             tf.summary.histogram(var.name, var.read_value())
 
@@ -330,5 +332,5 @@ class Model_ComiRec_SA(Model):
             self.mask: inps[3],
             self.lr: inps[4]
         }
-        summary = sess.run([self.merge_ops], feed_dict=feed_dict)
+        summary = sess.run(self.merge_ops, feed_dict=feed_dict)
         return summary

src/train.py
@@ -14,7 +14,7 @@ import faiss
 import tensorflow as tf
 from data_iterator import DataIterator
 from model import *
-from tensorboardX import SummaryWriter
+# from tensorboardX import SummaryWriter
 
 parser = argparse.ArgumentParser()
 parser.add_argument('-p', type=str, default='train', help='train | test')
@@ -220,8 +220,6 @@ def train(
 
     gpu_options = tf.GPUOptions(allow_growth=True)
 
-    writer = SummaryWriter('runs/' + exp_name)
-
     item_cate_map = load_item_cate(cate_file)
 
     with tf.Session(config=tf.ConfigProto(gpu_options=gpu_options)) as sess:
@@ -229,7 +227,9 @@ def train(
         valid_data = DataIterator(valid_file, batch_size, maxlen, train_flag=1)
         
         model = get_model(dataset, model_type, item_count, batch_size, maxlen)
-        
+
+        writer = tf.summary.FileWriter('runs/' + exp_name, graph=tf.get_default_graph())
+
         sess.run(tf.global_variables_initializer())
         sess.run(tf.local_variables_initializer())
 
@@ -261,15 +261,10 @@ def train(
                     print(exp_name)
                     print(log_str)
 
-                    writer.add_scalar('train/loss', loss_sum / test_iter, iter)
+                    tf.summary.scalar('train/loss', loss_sum / test_iter, iter)
                     if metrics != {}:
                         for key, value in metrics.items():
-                            writer.add_scalar('eval/' + key, value, iter)
-
-
-                    gradient_w = model.opt.compute_gradients(loss, var_list=tf.trainable_variables())
-                    for idx, itr_g, var in enumerate(zip(gradient_w, tf.trainable_variables())):
-                        writer.add_histogram(var.name, itr_g, iter)
+                            tf.summary.scalar('eval/' + key, value, iter)
 
 
                     if 'recall' in metrics: